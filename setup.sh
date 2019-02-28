8b0483ee81c54e93f41aa2f0d0ae1cbcb0736e4d
iarp <ian@iarp.ca>
2019-02-28 14:56:05 -0500

    Missed -i option of sed for in-place replacement, added comments to explain sed commands.

--- setup.sh
+++ setup.sh
@@ -7,4 +7,9 @@ rm -f .gitignore_global
 wget https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -O .gitignore_global
 rm -f .gitconfig
 wget https://raw.githubusercontent.com/iarp/dotfiles/master/.gitconfig
-sed -i 's/username/$USER/g' .gitconfig
+
+# Replace the username in the gitconfig file with the currently logged in user
+sed -i 's/username/$USER/g' .gitconfig -i
+
+# Enable bash colouring by uncommenting force_color_prompt
+sed '/force_color_prompt=yes/s/^# *//' .bashrc -i
