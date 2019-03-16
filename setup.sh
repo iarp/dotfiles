c26daf6d6535b22d7403055be3a31f568b829af0
iarp <ian@iarp.ca>
2019-03-15 22:04:31 -0400

    Remove duplicated -i option on sed and wrap in double quotes for shell variable replacement.

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ rm -f .gitconfig
 wget https://raw.githubusercontent.com/iarp/dotfiles/master/.gitconfig
 
 # Replace the username in the gitconfig file with the currently logged in user
-sed -i 's/username/$USER/g' .gitconfig -i
+sed "s/username/$USER/g" .gitconfig -i
 
 # Enable bash colouring by uncommenting force_color_prompt
 sed '/force_color_prompt=yes/s/^# *//' .bashrc -i
