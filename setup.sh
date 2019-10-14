7da07863c9848a41b7c346e1d0fd65ef70bfc158
iarp <ian@iarp.ca>
2019-10-14 13:49:22 -0400

    Add comments and command to install screen on first run.

--- setup.sh
+++ setup.sh
@@ -1,29 +1,37 @@
 cd ~
+
 rm -f .screenrc
-wget https://raw.githubusercontent.com/iarp/dotfiles/master/.screenrc
 rm -f .vimrc
-wget https://raw.githubusercontent.com/iarp/dotfiles/master/.vimrc
 rm -f .gitignore_global
-wget https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -O .gitignore_global
 rm -f .gitconfig
+
+wget https://raw.githubusercontent.com/iarp/dotfiles/master/.screenrc
+wget https://raw.githubusercontent.com/iarp/dotfiles/master/.vimrc
+wget https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -O .gitignore_global
 wget https://raw.githubusercontent.com/iarp/dotfiles/master/.gitconfig
+wget https://raw.githubusercontent.com/iarp/dotfiles/master/.bashrc -O .tmp_bashrc
 
 # Replace the username in the gitconfig file with the currently logged in user
+echo "Set username in gitconfig to $USER"
 sed "s/username/$USER/g" .gitconfig -i
 
 # Enable bash colouring by uncommenting force_color_prompt
+echo "Enable bash line colour prompt"
 sed '/force_color_prompt=yes/s/^# *//' .bashrc -i
 
 # Remove lines from .bashrc that start with the following
+echo "Removing HISTSIZE, HISTFILESIZE, HISTCONTROL, PROMPT_COMMAND from .bashrc"
 sed '/^HISTSIZE/d' .bashrc -i
 sed '/^HISTFILESIZE/d' .bashrc -i
 sed '/^HISTCONTROL/d' .bashrc -i
 sed '/^PROMPT_COMMAND/d' .bashrc -i
 
-wget https://raw.githubusercontent.com/iarp/dotfiles/master/.bashrc -O .tmp_bashrc
+echo "Copying commands from .tmp_bashrc to .bashrc"
 cat .tmp_bashrc >> .bashrc
 
-sudo apt update 
-sudo apt upgrade
+echo "Generating ssh key"
+ssh-keygen -t rsa -C "$USER@$HOSTNAME"
 
-ssh-keygen -t rsa -C "$HOSTNAME"
+sudo apt update
+sudo apt install -y screen
+sudo apt upgrade
